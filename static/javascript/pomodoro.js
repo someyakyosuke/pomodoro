0074d7a975e675da54771fb66fd2d83bb81b8785
someyakyosuke <someya226@yahoo.co.jp>
2021-02-03 17:28:08 +0900

    timer-reiauto

--- static/javascript/pomodoro.js
+++ static/javascript/pomodoro.js
@@ -1,67 +1,113 @@
-var timer1; //タイマーを格納する変数（タイマーID）の宣言
+var start = document.getElementById('start');
+//var stop = document.getElementById('stop');
+var reset = document.getElementById('reset');
+//work時間と休憩時間の切り替えフラグ
+var timeflg = 0;
+//ポモドーロのカウンター
+var count = 0;
+
+var min=15
+var sec=0
+
+var minutes = document.getElementById('minutes');
+var seconds= document.getElementById('seconds');
+
+//store a reference to a timer variable
+var startTimer;
+
+start.addEventListener('click', function(){
+    if(startTimer === undefined){
+        start.innerHTML="stop"
+        startTimer = setInterval(timer, 1000)
+    } else {
+       stopInterval()
+       startTimer = undefined;
+       start.innerHTML="start"
+    }
+})
+
+reset.addEventListener('click', function(){
+    start.innerHTML="start"
+    minutes.innerText = 15;
+    seconds.innerText = "00";
+  //document.getElementById('counter').innerText = 0;
+    stopInterval()
+    startTimer = undefined;
+})
 
 
-//カウントダウン関数を1000ミリ秒毎に呼び出す関数
-function cntStart()
-{
-  document.timer.elements[2].disabled=true;
-  timer1=setInterval("countDown()",1000);
-}
 
-//タイマー停止関数
-function cntStop()
-{
-  document.timer.elements[2].disabled=false;
-  clearInterval(timer1);
-}
 
-//カウントダウン関数
-function countDown()
-{
-  var min=document.timer.elements[0].value;
-  var sec=document.timer.elements[1].value;
-  
-  if( (min=="") && (sec=="") )
-  {
-    alert("時刻を設定してください！");
-    reSet();
-  }
-  else
-  {
-    if (min=="") min=0;
-    min=parseInt(min);
+//Start Timer Function
+function timer(){
+   
+    //Work Timer Countdown
+    if(sec != 0){
+        sec--;
+        if(sec<10){
+          seconds.innerText = "0" + sec;
+        }else{
+          seconds.innerText = sec;
+        }
+    } else if(minutes.innerText != 0 && seconds.innerText == 0){
+        sec = 59;
+        seconds.innerText = 59;
+        minutes.innerText--;
+    }
+
     
-    if (sec=="") sec=0;
-    sec=parseInt(sec);
-    
-    tmWrite(min*60+sec-1);
-  }
+
+    //Increment Counter by one if one full cycle is completed
+    if(minutes.innerText == 0 && seconds.innerText == "00"){
+        if(timeflg==0){
+            if(count==4){
+                minutes.innerText = 25;
+                seconds.innerText = "00";
+                timeflg = 1;
+            }else{
+                minutes.innerText = 5;
+                seconds.innerText = "00";
+                timeflg = 1;
+            }
+        }else{
+            minutes.innerText = 15;
+            seconds.innerText = "00";
+            timeflg = 0;
+            count++;
+        }
+        //document.getElementById('counter').innerText++;
+    }
 }
 
-//残り時間を書き出す関数
-function tmWrite(int)
-{
-  int=parseInt(int);
-  
-  if (int<=0)
-  {
-    reSet();
-    alert("時間です！");
-  }
-  else
-  {
-    //残り分数はintを60で割って切り捨てる
-    document.timer.elements[0].value=Math.floor(int/60);
-    //残り秒数はintを60で割った余り
-    document.timer.elements[1].value=int % 60;
-  }
+//Stop Timer Function
+function stopInterval(){
+    clearInterval(startTimer);
 }
 
-//フォームを初期状態に戻す（リセット）関数
-function reSet()
-{
-  document.timer.elements[0].value="0";
-  document.timer.elements[1].value="0";
-  document.timer.elements[2].disabled=false;
-  clearInterval(timer1);
-}  
+//ここから図形の描画
+onload = function() {
+    draw();
+  };
+   
+  function draw() {
+    var canvas = document.getElementById('rectangle');
+    const dpr = window.devicePixelRatio || 1;
+    width = window.innerWidth;
+    height = window.innerHeight;
+
+    canvas.width = width*dpr;
+    canvas.height = height*dpr;
+    if ( ! canvas || ! canvas.getContext ) {
+      return false;
+    }
+    var cvs = canvas.getContext('2d');
+   
+    /* rectangle */
+    cvs.strokeStyle = "blue";
+    cvs.lineWidth = 20;
+    cvs.beginPath(); /* 図形を描き始めることを宣言 */
+    cvs.scale(1,.45);
+    cvs.arc(900, 640, 580, 0 * Math.PI / 180, 360 * Math.PI / 180, false);
+    cvs.closePath(); /* 描いた線を閉じる */
+    cvs.stroke(); /* 描いた図形を線で表示させる */
+  }
