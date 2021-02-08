89b5f77fb5a131b0ea26505a9c01a4e70255d5a0
someyakyosuke <someya226@yahoo.co.jp>
2021-02-08 20:33:24 +0900

    pomodoro-kansei1

--- static/javascript/pomodoro.js
+++ static/javascript/pomodoro.js
@@ -15,6 +15,11 @@ var seconds= document.getElementById('seconds');
 //store a reference to a timer variable
 var startTimer;
 
+//楕円アニメーション用の変数
+var canvas = document.getElementById('rectangle');
+var cvs = canvas.getContext('2d');
+var openradius = 0;
+
 start.addEventListener('click', function(){
     if(startTimer === undefined){
         start.innerHTML="stop"
@@ -29,10 +34,14 @@ start.addEventListener('click', function(){
 reset.addEventListener('click', function(){
     start.innerHTML="start"
     minutes.innerText = 15;
+    min=15;
     seconds.innerText = "00";
+    sec=0;
   //document.getElementById('counter').innerText = 0;
-    stopInterval()
+    stopInterval();
     startTimer = undefined;
+    openradius = 0;
+    draw();
 })
 
 
@@ -49,29 +58,42 @@ function timer(){
         }else{
           seconds.innerText = sec;
         }
-    } else if(minutes.innerText != 0 && seconds.innerText == 0){
+    } else if(minutes.innerText != 0 && sec == 0){
         sec = 59;
         seconds.innerText = 59;
-        minutes.innerText--;
+        min--;
+        if(min<10){
+            minutes.innerText = "0" + min
+        }else{
+           minutes.innerText = min;
+        }
     }
-
+    //楕円のアニメーション設定
+    circle();
     
 
     //Increment Counter by one if one full cycle is completed
-    if(minutes.innerText == 0 && seconds.innerText == "00"){
+    if(minutes.innerText == 0 && sec == 0){
         if(timeflg==0){
-            if(count==4){
+            if(count==3){
                 minutes.innerText = 25;
+                min=25;
                 seconds.innerText = "00";
-                timeflg = 1;
+                sec=0;
+                timeflg = 2;
+                count=0;
             }else{
-                minutes.innerText = 5;
+                minutes.innerText = "05";
+                min=5;
                 seconds.innerText = "00";
+                sec=0;
                 timeflg = 1;
             }
         }else{
             minutes.innerText = 15;
+            min=15;
             seconds.innerText = "00";
+            sec=0;
             timeflg = 0;
             count++;
         }
@@ -88,26 +110,50 @@ function stopInterval(){
 onload = function() {
     draw();
   };
-   
-  function draw() {
-    var canvas = document.getElementById('rectangle');
-    const dpr = window.devicePixelRatio || 1;
-    width = window.innerWidth;
-    height = window.innerHeight;
 
-    canvas.width = width*dpr;
-    canvas.height = height*dpr;
+
+
+  function draw() {
+    
+
+    canvas.width = 1822;
+    canvas.height = 796;
     if ( ! canvas || ! canvas.getContext ) {
       return false;
     }
-    var cvs = canvas.getContext('2d');
-   
+    
     /* rectangle */
-    cvs.strokeStyle = "blue";
+    cvs.strokeStyle = "yellow";
     cvs.lineWidth = 20;
-    cvs.beginPath(); /* 図形を描き始めることを宣言 */
+    cvs.beginPath(); /* 図形を描き始めることを宣言(楕円) */
     cvs.scale(1,.45);
-    cvs.arc(900, 640, 580, 0 * Math.PI / 180, 360 * Math.PI / 180, false);
-    cvs.closePath(); /* 描いた線を閉じる */
+    cvs.arc(910, 640, 580, openradius * Math.PI / 180, 360 * Math.PI / 180, false);
     cvs.stroke(); /* 描いた図形を線で表示させる */
+    cvs.strokeStyle = "#7fff00";
+    cvs.lineWidth = 10;
+    cvs.beginPath(); /* 図形を描き始めることを宣言(楕円) */
+    cvs.arc(910, 640, 560, 0 * Math.PI / 180, 360 * Math.PI / 180, false);
+    cvs.stroke(); /* 描いた図形を線で表示させる */
+    cvs.beginPath(); /* 図形を描き始めることを宣言(楕円) */
+    cvs.arc(910, 640, 600, 0 * Math.PI / 180, 360 * Math.PI / 180, false);
+    cvs.stroke(); /* 描いた図形を線で表示させる */
+  }
+
+  function circle(){
+    cvs.clearRect(0,0,1822,796);
+    if(openradius==(360)){
+        openradius == 0;
+    }else{
+        if(timeflg==0){
+            //15分×60秒
+            openradius = openradius + (360/(15*60));
+        }else if(timeflg==1){
+            //5分×60秒
+            openradius = openradius + (360/(5*60));
+        }else{
+            //25分×60秒
+            openradius = openradius + (360/(25*60));
+        }
+    }
+    draw();
   }
