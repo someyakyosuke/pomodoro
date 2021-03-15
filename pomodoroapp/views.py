ef5763db0a862103ea6c5d1fb086a78df01eb385
someyakyosuke <someya226@yahoo.co.jp>
2021-03-16 07:31:31 +0900

    complete graph

--- pomodoroapp/views.py
+++ pomodoroapp/views.py
@@ -10,7 +10,7 @@ from django.utils import timezone
 from django.utils.timezone import localtime # 追加（日本時間にするため）
 from datetime import datetime, date,timedelta
 
-#ajax用に追加
+#ラジオボタンの入力データを送信（ajax用に追加）
 class AjaxFormMixin(object):
     print('とりあえず動いてますよ')
     def form_invalid(self, form):
@@ -36,6 +36,8 @@ class AjaxFormMixin(object):
             response = super(AjaxFormMixin, self).form_valid(form)
             return response
 
+#グラフの表示データ切り替え用のクラス
+
 # Create your views here.
 class IndexView(AjaxFormMixin,generic.CreateView):
     template_name="index.html"
@@ -46,11 +48,32 @@ class IndexView(AjaxFormMixin,generic.CreateView):
 class GraphView(generic.TemplateView):
     template_name="graph.html"
     model = Focus
-    def get_context_data(self, **kwargs):
+    def get_context_data(self,**kwargs):
         # 継承元のメソッド呼び出し
         context = super().get_context_data(**kwargs) 
         today = localtime(timezone.now()).date()
         print(today)
         print('この上でタイムゾーンが出力されます')
         context['graph_data'] = Focus.objects.filter(user=self.request.user,start_at__date=today)
+        print(context)
         return context
+    def post(self,request,**kwargs):
+        if self.request.is_ajax():
+            button_day = request.POST.get('button_value')
+            print(button_day + "この値がdaysに入る")
+            today = localtime(timezone.now())+timedelta(days=int(button_day))
+            today_day = today.date()
+            print(today_day)
+            object_data= Focus.objects.filter(user=self.request.user,start_at__date=today_day)
+            print (object_data)
+            graph_timedata = []
+            graph_shuutyuudata = []
+            for item in object_data:
+                graph_timedata.append(item.start_at.strftime("%H:%M"))
+                graph_shuutyuudata.append(int(item.shuutyuudo))
+            print (graph_timedata)
+            print (graph_shuutyuudata)
+            return JsonResponse({'graph_time':graph_timedata,'graph_shuutyuu':graph_shuutyuudata},status=200)
+
+
+    
